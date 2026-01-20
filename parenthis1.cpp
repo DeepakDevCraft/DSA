d88bfc169856afb6a30f7046471e6e1c44a4f2f9
DeepakDevCraft <deekumar.d22@gmail.com>
2026-01-20 11:52:38 +0530

    solved parenthis

---
+++ parenthis1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isBalance(string str){
+
+    int open = 0;
+    int close = 0;
+
+    for(int i = 0 ;s[i]!='\0';i++){
+
+        if(str[i]==')'){
+            
+        }
+
+
+    }
+}
+
+int main(){
+
+    string str ;
+    cin>>str;
+
+    cout<<isBalance(str);
+
+    return 0;
+}
